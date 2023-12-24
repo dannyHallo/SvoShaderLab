65b5d39e0ba1c7f87d21afac3f09ebf0519f9f40
dannyHallo <ruitian0716@gmail.com>
2023-12-24 12:34:34 +0800

    fixed op< in Coor3D

--- src/utils/Coor.hpp
+++ src/utils/Coor.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
-struct Coor2D {
+struct Coor2D
+{
   int x;
   int y;
 };
 
-struct Coor3D {
+struct Coor3D
+{
   int x;
   int y;
   int z;
 
   bool operator==(const Coor3D &rhs) const { return (x == rhs.x) && (y == rhs.y) && (z == rhs.z); }
-  bool operator<(const Coor3D &rhs) const { return (x < rhs.x) || (y < rhs.y) || (z < rhs.z); }
+  // Define the operator< for Coor3D
+  bool operator<(const Coor3D &other) const {
+    // First compare by x, then by y, then by z
+    if (x != other.x)
+      return x < other.x;
+    if (y != other.y)
+      return y < other.y;
+    return z < other.z;
+  }
 
   Coor3D operator/(int s) const { return {x / s, y / s, z / s}; }
   Coor3D operator*(int s) const { return {x * s, y * s, z * s}; }
